300cf7ecf7b9cbc92145b3650aeb89a1030b559d
pavithrakumar22 <pavithrakumarsahu60@gmail.com>
2024-08-26 08:02:59 +0530

    util code

---
+++ src/db/index.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+ export const connectDB = async () =>{
+    try {
+        const connectionInstance=await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log(` \n Mongodb connected!! DB HOST : ${connectionInstance.connection.host}`)
+    } catch (error) {
+        console.log("Mongodb connection error: ",error)
+        process.exit(1);
+    }
+}
---
+++ src/index.js
@@ -0,0 +1,52 @@
+import mongoose from "mongoose";
+// import { DB_NAME } from "./constants";
+// import { connectDB } from "./db";
+
+import dotenv from "dotenv"
+import {connectDB} from "./db/index.js"
+
+dotenv.config({
+    path: "./env"
+})
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import express from "express"
+// const app = express()
+
+// function connectDb(){}
+
+// connectDb()
+
+// ;(async ()=>{
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//         app.on("error",(error)=>{
+//             console.log("ERROR: ",error);
+//             throw error
+//         })
+
+//         app.listen(process.env.PORT,()=>{
+//             console.log(`App is listening on port ${process.env.PORT}`)
+//         })
+//     } catch (error) {
+//         console.error("ERROR: ",error)
+//         throw err
+//     }
+// })()
